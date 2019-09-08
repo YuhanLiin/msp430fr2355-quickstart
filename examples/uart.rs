6a46d873e37cca549398951b3d79b22f5d96ba1d
YuhanLiin <linyuhan0315@hotmail.com>
2019-09-08 13:47:48 -0400

    Got uart echo working

--- examples/uart.rs
+++ examples/uart.rs
@@ -7,18 +7,39 @@ extern crate panic_msp430;
 
 use core::cell::RefCell;
 use msp430::interrupt;
-use msp430fr2355::Peripherals;
+use msp430fr2355::{Peripherals, E_USCI_A1};
 
 static PERIPHERALS: interrupt::Mutex<RefCell<Option<Peripherals>>> =
     interrupt::Mutex::new(RefCell::new(None));
 
+// Print ASCII character synchronously, not meant to be called directly
+fn transmit_byte(uart: &E_USCI_A1, ch: u8) {
+    while uart.uca1ifg().read().uctxifg().is_uctxifg_0() {}
+    uart.uca1txbuf().write(|w| unsafe { w.uctxbuf().bits(ch) });
+}
+
+fn transmit_char(uart: &E_USCI_A1, ch: u8) {
+    match ch as char {
+        '\n' | '\r' => {
+            transmit_byte(uart, '\r' as u8);
+            transmit_byte(uart, '\n' as u8);
+        }
+        _ => transmit_byte(uart, ch),
+    }
+}
+
+fn transmit_str(uart: &E_USCI_A1, s: &str) {
+    for ch in s.bytes() {
+        transmit_char(uart, ch);
+    }
+}
+
 fn main() {
     let peripherals = Peripherals::take().unwrap();
     let uart = &peripherals.E_USCI_A1;
-    let p1 = &peripherals.P1;
-    let p6 = &peripherals.P6;
     let p4 = &peripherals.P4;
 
+    // Turn off watchdog
     peripherals
         .WDT_A
         .wdtctl
@@ -28,49 +49,28 @@ fn main() {
     // Set ACLK to REFO (32.768 kHz)
     peripherals.CS.csctl4.write(|w| w.sela().refoclk());
 
-    p1.p1dir.write(|w| unsafe { w.bits(0xFF) });
-    p6.p6dir.write(|w| unsafe { w.bits(0xFF) });
-    p1.p1out.write(|w| unsafe { w.bits(0x00) });
-    p6.p6out.write(|w| unsafe { w.bits(0x00) });
-
-    uart.uca1ctlw0().write(|w| w.ucswrst().enable());
+    p4.p4sel0.write(|w| unsafe { w.bits((1 << 3) | (1 << 2)) });
     {
-        // Sets parity to even, keep everything else to default (8-bit data, LSB 1st, 1 stop bit)
-        // Use ACLK (32.768 kHz) for clock source
-        uart.uca1ctlw0()
-            .write(|w| w.ucpen().set_bit().ucpar().even().ucssel().aclk());
-        // Should setup a baud rate of 9600
+        uart.uca1ctlw0().write(|w| w.ucswrst().enable());
+        // Keep everything else to default (8-bit data, LSB 1st, 1 stop bit, no parity)
+        // Use ACLK (32.768 kHz) for clock source to set baud rate of 9600
         uart.uca1mctlw
             .write(|w| unsafe { w.ucos16().clear_bit().ucbrs().bits(0x92) });
         uart.uca1brw().write(|w| unsafe { w.bits(3) });
+        // Need to set CTLW0 here or else this write will wipe out previous CTLW0 settings, such as
+        // the clock setting, causing everything to go to hell
+        uart.uca1ctlw0()
+            .write(|w| w.ucswrst().disable().ucssel().aclk());
     }
-    uart.uca1ctlw0().write(|w| w.ucswrst().disable());
-    p4.p4sel0.write(|w| unsafe { w.bits((1 << 2) | (1 << 3)) });
 
-    uart.uca1ie()
-        .write(|w| w.uctxie().set_bit().ucrxie().set_bit());
+    transmit_str(uart, "hello world\n");
 
+    uart.uca1ie().write(|w| w.ucrxie().set_bit());
     interrupt::free(|cs| {
         *PERIPHERALS.borrow(cs).borrow_mut() = Some(peripherals);
     });
-
     unsafe { interrupt::enable() };
     loop {}
-
-    //for i in 0..100000 {
-    //if i > 0 {
-    //p6.p6out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-    //}
-    //while uart.uca1ifg().read().uctxifg().is_uctxifg_0() {}
-    //p1.p1out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-    //uart.uca1txbuf()
-    //.write(|w| unsafe { w.uctxbuf().bits('H' as u8) });
-
-    //while uart.uca1ifg().read().ucrxifg().is_ucrxifg_0() {}
-    //p1.p1out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-    //p6.p6out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-    //uart.uca1rxbuf().read().ucrxbuf();
-    //}
 }
 
 interrupt!(EUSCI_A1, uart_handler);
@@ -78,24 +78,15 @@ fn uart_handler() {
     interrupt::free(|cs| {
         let peripherals_ref = &*PERIPHERALS.borrow(cs).borrow();
         let peripherals = peripherals_ref.as_ref().unwrap();
-
-        let p1 = &peripherals.P1;
-        let p6 = &peripherals.P6;
         let uart = &peripherals.E_USCI_A1;
 
         let iv = uart.uca1iv().read().uciv();
         if iv.is_ucrxifg() {
-            p6.p6out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-            uart.uca1rxbuf().read().ucrxbuf();
-        } else if iv.is_uctxifg() {
-            p1.p1out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-            uart.uca1txbuf()
-                .write(|w| unsafe { w.uctxbuf().bits('H' as u8) });
+            // Echo the input back
+            let c = uart.uca1rxbuf().read().ucrxbuf().bits();
+            transmit_char(uart, c);
         } else {
-            p6.p6out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-            p1.p1out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-            p6.p6out.modify(|r, w| unsafe { w.bits(!r.bits()) });
-            p1.p1out.modify(|r, w| unsafe { w.bits(!r.bits()) });
+            transmit_str(uart, "WTF wrong interrupt\n");
         }
     });
 }
